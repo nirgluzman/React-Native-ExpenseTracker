d116703bc0b7c15fc5fd2748d091194ad463bb91
nirgluzman <nir.gluzman@gmail.com>
2024-05-22 01:00:25 +0200

    add helper function to convert date string to JavaScript Date object format

--- util/date.js
+++ util/date.js
@@ -20,6 +20,19 @@ export function getFormattedDate(date) {
   return `${('0' + date.getDate()).slice(-2)}-${monthNames[date.getMonth()]}-${date.getFullYear()}`;
 }
 
+// convert the input date string (DD.MM.YYYY) to JavaScript Date object format (YYYY-MM-DD).
+export function convertDateString(dateString) {
+  // Split the string using a dot (.) as delimiter.
+  const parts = dateString.split('.');
+
+  const year = parts[2];
+  const month = parts[1];
+  const day = parts[0];
+
+  // return the date in JavaScript Date object format (YYYY-MM-DD).
+  return `${year}-${month}-${day}`;
+}
+
 // calculate the date of the day which is 'days' before 'date'.
 export function getDateMinusDays(date, days) {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
