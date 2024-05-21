7ca4cc11412c88d41eaea8074a1b0513199cc972
nirgluzman <nir.gluzman@gmail.com>
2024-05-21 18:51:57 +0200

    remove DUMMY_EXPENSES

--- components/Expenses/ExpensesOutput.js
+++ components/Expenses/ExpensesOutput.js
@@ -6,44 +6,11 @@ import ExpensesList from './ExpensesList.js';
 // global styles for the project
 import { GlobalStyles } from '../../constants/styles.js';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-19')
-  },
-  {
-    id: 'e2',
-    description: 'pair of trousers',
-    amount: 89.99,
-    date: new Date('2022-01-05')
-  },
-  {
-    id: 'e3',
-    description: 'Bananas',
-    amount: 5.99,
-    date: new Date('2023-02-01')
-  },
-  {
-    id: 'e4',
-    description: 'Book',
-    amount: 15.99,
-    date: new Date('2022-06-08')
-  },
-  {
-    id: 'e5',
-    description: 'Book',
-    amount: 18.99,
-    date: new Date('2024-09-01')
-  }
-];
-
 export default function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList expenses={expenses} />
     </View>
   );
 }
