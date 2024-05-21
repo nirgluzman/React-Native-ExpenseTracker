20d7691f06dfe023432e8c1134cd322a3d156a94
nirgluzman <nir.gluzman@gmail.com>
2024-05-21 19:12:05 +0200

    add hook to access the values stored in React context

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,5 +1,10 @@
+import { useContext } from 'react';
+
+import { ExpensesContext } from '../store/expenses-context.js';
 import ExpensesOutput from '../components/Expenses/ExpensesOutput.js';
 
 export default function AllExpenses() {
-  return <ExpensesOutput expensesPeriod='Total' />;
+  const expensesCtx = useContext(ExpensesContext);
+
+  return <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod='Total' />;
 }
