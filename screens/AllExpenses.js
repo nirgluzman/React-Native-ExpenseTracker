a5af8e1ec3cab548edd849cfa62aa4d6188e9c28
nirgluzman <nir.gluzman@gmail.com>
2024-05-22 11:02:52 +0200

    move useEffect/fetchExpenses to AllExpenses as this is the route to render on first load of the navigator

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,11 +1,27 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { ExpensesContext } from '../store/expenses-context.js';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 
+// helper function to fetch expenses from database.
+import { fetchExpenses } from '../util/http.js';
+
 export default function AllExpenses() {
   // hook that allows the component to access the values stored in React context.
   const expensesCtx = useContext(ExpensesContext);
+
+  // fetch all expenses from database; will only run after the initial render.
+  useEffect(
+    () => {
+      async function getExpenses() {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpenses(expenses); // update the state of the context with expenses fetched from the database.
+      }
+
+      getExpenses();
+    },
+    [] // to run once on initial render.
+  );
 
   return (
     <ExpensesOutput
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -3,26 +3,12 @@ import { useEffect, useContext } from 'react';
 import { ExpensesContext } from '../store/expenses-context.js';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 
-// helper functions
+// helper function
 import { getDateMinusDays } from '../util/date.js';
-import { fetchExpenses } from '../util/http.js';
 
 export default function RecentExpenses() {
   // hook that allows the component to access the values stored in React context.
   const expensesCtx = useContext(ExpensesContext);
-
-  // fetch all expenses from database; will only run after the initial render.
-  useEffect(
-    () => {
-      async function getExpenses() {
-        const expenses = await fetchExpenses();
-        expensesCtx.setExpenses(expenses); // update the state of the context with expenses fetched from the database.
-      }
-
-      getExpenses();
-    },
-    [] // to run once on initial render.
-  );
 
   // extract the expenses that were created in the last 7 days.
   const recentExpenses = expensesCtx.expenses.filter(expense => {
