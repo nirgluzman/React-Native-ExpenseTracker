b91efb30ce6dd99ec6f4c493aee0389871b5a1fa
nirgluzman <nir.gluzman@gmail.com>
2024-05-22 11:36:29 +0200

    add loading spinner

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,12 +1,15 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { ExpensesContext } from '../store/expenses-context.js';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import LoadingOverlay from '../components/UI/LoadingOverlay'; // loading spinner custom component
 
-// helper function to fetch expenses from database.
+// helper functions to handle HTTP requests to Firebase Realtime database.
 import { fetchExpenses } from '../util/http.js';
 
 export default function AllExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
+
   // hook that allows the component to access the values stored in React context.
   const expensesCtx = useContext(ExpensesContext);
 
@@ -14,7 +17,9 @@ export default function AllExpenses() {
   useEffect(
     () => {
       async function getExpenses() {
-        const expenses = await fetchExpenses();
+        setIsFetching(true);
+        const expenses = await fetchExpenses(); // await only defers execution of code that actually depends on the result, i.e. anything after the await expression.
+        setIsFetching(false);
         expensesCtx.setExpenses(expenses); // update the state of the context with expenses fetched from the database.
       }
 
@@ -22,6 +27,11 @@ export default function AllExpenses() {
     },
     [] // to run once on initial render.
   );
+
+  // display a loading spinner while expenses are being fetched from Firebase.
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
 
   return (
     <ExpensesOutput
