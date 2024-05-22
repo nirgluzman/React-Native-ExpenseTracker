fe80f9bf3c3ce16112adfc3c6e9fde9fec4b6290
nirgluzman <nir.gluzman@gmail.com>
2024-05-22 12:04:48 +0200

    display an error message if an error occurred while fetching expenses

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,14 +1,18 @@
 import { useContext, useEffect, useState } from 'react';
 
-import { ExpensesContext } from '../store/expenses-context.js';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import LoadingOverlay from '../components/UI/LoadingOverlay'; // loading spinner custom component
+import ErrorOverlay from '../components/UI/ErrorOverlay'; // error message custom component
+
+// React context
+import { ExpensesContext } from '../store/expenses-context.js';
 
 // helper functions to handle HTTP requests to Firebase Realtime database.
 import { fetchExpenses } from '../util/http.js';
 
 export default function AllExpenses() {
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
 
   // hook that allows the component to access the values stored in React context.
   const expensesCtx = useContext(ExpensesContext);
@@ -17,16 +21,30 @@ export default function AllExpenses() {
   useEffect(
     () => {
       async function getExpenses() {
-        setIsFetching(true);
-        const expenses = await fetchExpenses(); // await only defers execution of code that actually depends on the result, i.e. anything after the await expression.
-        setIsFetching(false);
-        expensesCtx.setExpenses(expenses); // update the state of the context with expenses fetched from the database.
+        setIsFetching(true); // enable loading spinner.
+        setError(null); // reset error message.
+        try {
+          const expenses = await fetchExpenses(); // await only defers execution of code that actually depends on the result, i.e. anything after the await expression.
+          expensesCtx.setExpenses(expenses); // update the state of the context with expenses fetched from the database.
+        } catch (error) {
+          console.log(error);
+          setError('Could not fetch expenses!'); // set error message.
+        }
+        setIsFetching(false); // disable loading spinner.
       }
 
       getExpenses();
     },
     [] // to run once on initial render.
   );
+
+  // display an error message if an error occurred while fetching expenses.
+  function errorHandler() {
+    setError(null);
+  }
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} onConfirm={errorHandler} />;
+  }
 
   // display a loading spinner while expenses are being fetched from Firebase.
   if (isFetching) {
